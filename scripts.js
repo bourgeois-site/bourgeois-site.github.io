78095fe4076a3d732c63cd42d50c8e1bcde964e6
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-10-06 16:18:24 +0300

    Template for jumbotron

--- scripts.js
+++ scripts.js
@@ -3,7 +3,14 @@ var roundThumbnail = function() {
   element.css('height', element.css('width'));
   element.css('border-radius', '50%');
 }
+
+var setHeightJumbotron = function() {
+  var windowHeight = $(window).height();
+  $('main #jumbotron').css('height', String(windowHeight) + "px");
+}
+
 $(document).ready(function() {
+  setHeightJumbotron();
   roundThumbnail();
   $(window).scroll(function() {
     if($(this).scrollTop() > 50) {
