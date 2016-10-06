1e38e0540ccf8611776e8a2e1be6b887a128dbad
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-10-06 14:49:13 +0300

    Better customers view

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
+var roundThumbnail = function() {
+  var element = $('main #customers .thumbnail');
+  element.css('height', element.css('width'));
+  element.css('border-radius', '50%');
+}
 $(document).ready(function() {
+  roundThumbnail();
   $(window).scroll(function() {
     if($(this).scrollTop() > 50) {
       $('header nav').addClass('reduced');
